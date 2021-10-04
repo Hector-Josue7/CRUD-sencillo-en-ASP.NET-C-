650984ac6e2269f1aa0ac70a4d2d5ce1ff3dea85
Hector-Josue7 <hectorardonm@gmail.com>
2021-10-03 23:03:25 -0600

    Subida de archivos

---
+++ ejemploCrudAsp/formularioCrud.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="formularioCrud.aspx.cs" Inherits="ejemploCrudAsp.formularioCrud" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    
+   
+    
+
+
+     <form id="form2" runat="server">
+        <div>
+            <h1>SELECT</h1>
+            <asp:Label ID="Label1" runat="server" Text="ID articulo"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server" Width="78px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Mostrar ID Mochilas" OnClick="Button1_Click"  />
+            <br />
+            <hr />
+            <h1>INSERT</h1>
+            <asp:Label ID="Label2" runat="server" Text="Ingrese el articulo: "></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:Label ID="Label6" runat="server" Text="Ingrese el precio"></asp:Label>
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            <asp:Button ID="Button2" runat="server" Text="Guardar" OnClick="Button2_Click"  />
+            <br />
+            <hr />
+            <h1>UPDATE</h1>
+            <asp:Label ID="Label3" runat="server" Text="Actualiza descripcion del ID producto = 5:"></asp:Label>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Nueva descripcion: "></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <asp:Label ID="Label7" runat="server" Text="Nuevo precio"></asp:Label>
+            <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+            <asp:Button ID="Button3" runat="server" Text="Actualizar" OnClick="Button3_Click"  />
+            <br />
+            <hr />
+            <h1>DELETE</h1>
+            <asp:Label ID="Label5" runat="server" Text="Borrar producto: "></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <asp:Button ID="Button4" runat="server" Text="Eliminar" OnClick="Button4_Click" />
+            <br />
+            <hr />
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ ejemploCrudAsp/formularioCrud.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ejemploCrudAsp
+{
+    public partial class formularioCrud : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {  
+            using (System.Data.SqlClient.SqlConnection openCon = new System.Data.SqlClient.SqlConnection("Data Source=HECTORJOSUE;Initial Catalog=tutorialCrud;Integrated Security=True"))
+            {
+                string saveStaff = "SELECT id_articulo from tbl_articulos where nombre = 'Mochilas'   ";
+
+                using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                {
+                    querySaveStaff.Connection = openCon;                    
+                    openCon.Open();
+                    SqlDataReader reader = querySaveStaff.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        TextBox1.Text = reader.GetSqlInt32(0).ToString();
+                    }
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection openCon = new SqlConnection("Data Source=HECTORJOSUE;Initial Catalog=tutorialCrud;Integrated Security=True"))
+            {
+                string saveStaff = "INSERT into tbl_articulos (nombre, precio) VALUES (@nombre, @precio)";
+
+                using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                {
+                    querySaveStaff.Connection = openCon;
+                    querySaveStaff.Parameters.Add("@nombre", SqlDbType.VarChar, 30).Value = TextBox2.Text;
+                    querySaveStaff.Parameters.Add("@precio", SqlDbType.VarChar, 30).Value = TextBox5.Text;
+                    openCon.Open();
+                    querySaveStaff.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+               using (SqlConnection openCon = new SqlConnection("Data Source=HECTORJOSUE;Initial Catalog=tutorialCrud;Integrated Security=True"))
+            {
+                string saveStaff = "UPDATE tbl_articulos SET nombre= @nombre, precio= @precio where id_articulo =6";
+
+                using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                {
+                    querySaveStaff.Connection = openCon;
+                    querySaveStaff.Parameters.Add("@nombre", SqlDbType.VarChar, 30).Value = TextBox3.Text;
+                    querySaveStaff.Parameters.Add("@precio", SqlDbType.Money).Value = TextBox6.Text;
+                    openCon.Open();
+                    querySaveStaff.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection openCon = new SqlConnection("Data Source=HECTORJOSUE;Initial Catalog=tutorialCrud;Integrated Security=True"))
+            {
+                string saveStaff = "DELETE FROM tbl_articulos WHERE id_articulo = @id_articulo";
+
+                using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                {
+                    querySaveStaff.Connection = openCon;
+                    querySaveStaff.Parameters.Add("@id_articulo", SqlDbType.VarChar, 30).Value = TextBox4.Text;
+                    openCon.Open();
+                    querySaveStaff.ExecuteNonQuery();
+                }
+            }
+
+        }
+    }
+    
+}
